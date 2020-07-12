6d2672130cdbba4b1b10c6c14adc989c2de5137c
Aneesh Relan <aneesh.relan@gmail.com>
2020-07-12 21:07:15 +0530

    Add cdk rules exception

---
+++ cdk.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    "functional/no-class": "off",
+    "@typescript-eslint/no-require-imports": "off",
+    "functional/no-this-expression": "off",
+    "no-console": "off",
+  }
+};
